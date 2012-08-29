f1693c60bb3dda3edae1b1cc10724860348d664a
Bouke Haarsma <bouke@webatoom.nl>
2012-08-29 12:05:15 +0200

    Updated version after fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='spa-phone-api',
-    version='0.1.0',
+    version='0.1.1',
     author='Bouke Haarsma',
     author_email='bouke@webatoom.nl',
     packages=[
