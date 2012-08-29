a7aba8a9cd162503663c1867eaeb2c39d8ddf2b2
Bouke Haarsma <bouke@webatoom.nl>
2012-08-29 17:12:54 +0200

    Functional sync command tool

--- spa_sync/__init__.py
+++ spa_sync/__init__.py
@@ -1,5 +1,19 @@
+from optparse import OptionParser
+import sys
 import spa_phone
+from spa_sync.providers.contacts import export
 
 def main():
-    print 'hello SPA SYNC! version', spa_phone.__version__
-    pass
+    parser = OptionParser()
+    parser.add_option("-g", "--group", dest="group",
+        help="OS X Contacts group name to sync")
+    (options, args) = parser.parse_args()
+
+    if len(args) == 0:
+        print 'usage: spa-sync ip-address'
+        print 'Please provide the SPA Phone IP address as first argument'
+        sys.exit(2)
+
+    contacts = export(options.group)
+    spa_phone.write(args[0], contacts)
+    print 'Synced', len(contacts), 'entries'
--- spa_sync/providers/contacts/__init__.py
+++ spa_sync/providers/contacts/__init__.py
@@ -4,7 +4,7 @@ for version in ['2.7', '2.6', '2.5']:
     sys.path.append('/System/Library/Frameworks/Python.framework/Versions/'
                     + version + '/Extras/lib/python/PyObjC')
 
-import addressbook
+from spa_sync.providers.contacts import addressbook
 
 __all__ = ['export', ]
 
@@ -14,10 +14,17 @@ def export(group=None):
     """
     contacts = []
     for contact in addressbook.getPeopleInAddressBook(group_name=group):
+        num_phones = 0
         for type, phone in contact.get('phone', []):
+            if not 'fax' in type.lower():
+                num_phones += 1
+        for type, phone in contact.get('phone', []):
+            if 'fax' in type.lower():
+                continue
             name = ' '.join([contact.get('first', ''),
+                             contact.get('middle', ''),
                              contact.get('last', '')]).strip()
-            if len(contact['phone']) > 1:
+            if num_phones > 1:
                 name += ' (%s)' % type
             contacts.append((name, phone))
     return contacts
