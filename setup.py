87aca682d745018ba9cf8653dd7fd0f727ff9e6f
Bouke Haarsma <bouke@webatoom.nl>
2012-08-29 17:48:40 +0200

    New release after bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import spa_phone
 
 setup(
     name='spa-phone-api',
-    version='0.2.0',
+    version='0.2.1',
     author='Bouke Haarsma',
     author_email='bouke@webatoom.nl',
     packages=find_packages(),
