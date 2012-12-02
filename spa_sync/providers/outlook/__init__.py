aafc295eca4c53a5ff50d988707f85e6b2213ddf
Arie Bounder <arie@bounder.nl>
2012-12-02 16:37:34 +0100

    Added outlook as a provider

---
+++ spa_sync/providers/outlook/__init__.py
@@ -0,0 +1,17 @@
+from spa_sync.providers.outlook.client import MSOutlook
+
+
+def export(group=None):
+    """
+    Export the contacts for this provider in the correct format
+    """
+    contacts = []
+    ms_client = MSOutlook()
+    for contact in ms_client.get_contacts():
+        name = contact['FullName'] or contact['CompanyName']
+        for key, value in contact['Phones'].items():
+            if len(contact['Phones']) > 1:
+                    type = key.replace("TelephoneNumber", "")
+                    name += ' (%s)' % type
+            contacts.append((name, value))
+    return contacts
---
+++ spa_sync/providers/outlook/client.py
@@ -0,0 +1,49 @@
+import win32com.client
+
+PHONE_FIELDS = ['BusinessTelephoneNumber',
+                'HomeTelephoneNumber',
+                'MobileTelephoneNumber',
+                #'AssistantTelephoneNumber',
+                #'OtherTelephoneNumber',
+                #'PrimaryTelephoneNumber',
+            ]
+
+NAME_FIELDS = ['FullName',
+                'CompanyName',
+            ]
+
+
+class MSOutlook(object):
+    def __init__(self):
+        self.outlookFound = 0
+        try:
+            self.oOutlookApp =\
+            win32com.client.gencache.EnsureDispatch("Outlook.Application")
+            self.outlookFound = 1
+        except:
+            print "unable to load Outlook"
+        self.records = []
+
+    def get_contacts(self):
+        #todo add a way to filter out contacts you don't want to sync
+        if not self.outlookFound:
+            return
+
+        mapi = self.oOutlookApp.GetNamespace("MAPI")
+        ofContacts =\
+        mapi.GetDefaultFolder(win32com.client.constants.olFolderContacts)
+
+        for oc in range(len(ofContacts.Items)):
+            contact = ofContacts.Items.Item(oc + 1)
+            if contact.Class == win32com.client.constants.olContact:
+                record = {}
+
+                for key in NAME_FIELDS:
+                    record[key] = getattr(contact, key)
+                    record['Phones'] = {}
+                    for pkey in PHONE_FIELDS:
+                        value = getattr(contact, pkey)
+                        if value:
+                            record['Phones'][pkey] = value
+                self.records.append(record)
+        return self.records
