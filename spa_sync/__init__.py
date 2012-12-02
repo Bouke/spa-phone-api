1456e4579d91476bd321f2d75ce3f3fdc2dc6f5d
Arie Bounder <arie@bounder.nl>
2012-12-02 16:38:10 +0100

    Added an option to provide which provider you would like to use.

--- spa_sync/__init__.py
+++ spa_sync/__init__.py
@@ -1,18 +1,25 @@
 from optparse import OptionParser
 import sys
 import spa_phone
-from spa_sync.providers.contacts import export
 
 def main():
     parser = OptionParser()
+    parser.add_option("-p", "--provider", dest="provider", default="contacts",
+        help="Contact provider to use: contacts | outlook. default=contacts")
     parser.add_option("-g", "--group", dest="group",
         help="OS X Contacts group name to sync")
+
     (options, args) = parser.parse_args()
 
     if len(args) == 0:
         print 'usage: spa-sync ip-address'
         print 'Please provide the SPA Phone IP address as first argument'
         sys.exit(2)
+
+    if options.provider == 'contacts':
+        from spa_sync.providers.contacts import export
+    if options.provider == 'outlook':
+        from spa_sync.providers.outlook import export
 
     contacts = export(options.group)
     spa_phone.write(args[0], contacts)
